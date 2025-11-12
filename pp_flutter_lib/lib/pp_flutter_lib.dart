f66e5e86b2fd8d30219eacff77177ae9381b31d9
liupenghui <liupenghui@58.com>
2025-11-12 15:31:23 +0800

    review

--- pp_flutter_lib/lib/pp_flutter_lib.dart
+++
@@ -1,8 +0,0 @@
-
-import 'pp_flutter_lib_platform_interface.dart';
-
-class PpFlutterLib {
-  Future<String?> getPlatformVersion() {
-    return PpFlutterLibPlatform.instance.getPlatformVersion();
-  }
-}
